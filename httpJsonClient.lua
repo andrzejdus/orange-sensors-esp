287536d6addb49077ae2e32cd4405b1641c9dad1
Andrzej Duś <andrzej.dus@gmail.com>
2017-11-12 13:40:29 +0100

    Fix http headers

--- httpJsonClient.lua
+++ httpJsonClient.lua
@@ -1,6 +1,6 @@
 function get(baseUrl, urlPath, finishedCallback)
     local url = string.format('%s/%s', baseUrl, urlPath);
-    local headers = 'Content-Type: application/json\r\n';
+    local headers = 'Content-Type: application/json\n';
     
     print(string.format('Getting data from %s', url));
     http.get(url, nil, function(code, data)
@@ -10,7 +10,7 @@ function get(baseUrl, urlPath, finishedCallback)
         else
             decodedData = sjson.decode(data);
         end
-        print(code, data);
+        print('Response', data);
 
         finishedCallback(code, decodedData);
     end);
@@ -18,21 +18,21 @@ end
 
 function post(baseUrl, urlPath, body, finishedCallback)
     local url = string.format('%s/%s', baseUrl, urlPath);
-    local headers = 'Content-Type: application/json\r\n';
---    local headers = '';
+    local headers = 'Content-Type: application/json\n';
     
     print(string.format('Sending data to %s', url));
     local body = sjson.encode(body);
     print('Body', body);
+
 --    if (false) then
     http.post(url, headers, body, function(code, data)
         local decodedData = nil;
         if (code ~= 200) then
             print(string.format('HTTP request failed, status %s', code));
         else
-            print('Received', data);
             decodedData = sjson.decode(data);
         end
+        print('Response', data);
 
         finishedCallback(code, decodedData);
     end);
