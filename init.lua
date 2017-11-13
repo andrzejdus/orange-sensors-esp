f7cc911e709492f52a2930ee5be104eff186cf05
Andrzej Duś <andrzej.dus@gmail.com>
2017-11-13 18:03:57 +0100

    Update init script

--- init.lua
+++ init.lua
@@ -1,13 +1,8 @@
-function startup()
-    if file.open('init.lua') == nil then
-        print('init.lua deleted')
-    else
-        print('init.lua ok - running')
-        
-        file.close('init.lua')
+local delay = 3000;
 
-        dofile('app.lua')
-    end
-end
-
-tmr.alarm(0, 2500, 0, startup)
+print('---------------------------');
+print(string.format('Waiting %s ms before running app.lua (you can delete it now)', delay));
+tmr.alarm(0, delay, 0, function()
+    print('Starting app.lua');
+    dofile('app.lua')
+end);
