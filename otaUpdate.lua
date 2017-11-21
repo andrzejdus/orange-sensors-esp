25255c8f2854fb058db45617abc944c9dad32faf
Andrzej Duś <andrzej.dus@gmail.com>
2017-11-22 00:01:13 +0100

    More ota experiments

--- otaUpdate.lua
+++ otaUpdate.lua
@@ -3,10 +3,9 @@ local otaUpdate = {};
 function otaUpdate.create()
     local export = {};
 
-    local OTA_BASE_URL = 'https://api.github.com';
+    local OTA_BASE_URL = 'https://api.github.com/repos/andrzejdus/orange-sensors-esp/contents';
 
-    function export.listFiles()
-        local url = string.format('%s/%s', OTA_BASE_URL, 'repos/andrzejdus/orange-sensors-esp/contents/');
+    function getFile(url, onSuccess)
         local headers = 'Content-Type: application/json\n';
 
         print(string.format('Getting file list from %s', url));
@@ -14,12 +13,28 @@ function otaUpdate.create()
             if (code ~= 200) then
                 print(string.format('HTTP request failed, status %s', code));
             else
-                local decodedBody = sjson.decode(body);
+                onSuccess(body);
+            end
+        end);
+    end
 
-                print(body);
-                for key, value in pairs(decodedBody) do
-                    print(key, value);
-                end
+    function getGitHubFile(filename, onSuccess)
+        getFile(string.format('%s/%s', OTA_BASE_URL, filename), 
+                function (body)
+            local decodedBody = sjson.decode(body);
+
+            getFile(decodedBody.download_url, onSuccess);
+        end);
+    end
+
+    function export.listFiles()
+        getGitHubFile('ota-files.json', function (body)
+            local filesList = sjson.decode(body);
+            for key, filename in pairs(filesList) do
+                print('Getting', filename);
+                getGitHubFile(filename, function (body)
+                    print(body);
+                end);
             end
         end);
     end
