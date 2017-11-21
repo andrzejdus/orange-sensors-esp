e85800e9c4db6d86e54823657f28c9210c9b2f67
Andrzej Duś <andrzej.dus@gmail.com>
2017-11-21 23:34:51 +0100

    Clean config file

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 return {
-    STATUS_DIODE_PIN = 1,
     BASE_URL = 'http://orange-sensors-web.us-east-1.elasticbeanstalk.com',
-    TRIGGER_PIN = 6,
-    ECHO_PIN = 7
+    STATUS_DIODE_PIN = 1, -- GPIO5
+    TRIGGER_PIN = 6, -- GPIO12
+    ECHO_PIN = 7 -- GPIO13 
 };
