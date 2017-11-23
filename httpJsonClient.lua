2734ab121852186277c91085a635220124ec4b4e
Andrzej Duś <andrzej.dus@gmail.com>
2017-11-23 22:47:52 +0100

    Clean http client

--- httpJsonClient.lua
+++ httpJsonClient.lua
@@ -15,24 +15,20 @@ end
 
 function get(baseUrl, urlPath, expectedStatus, finishedCallback)
     local url = string.format('%s/%s', baseUrl, urlPath);
-    local headers = 'Content-Type: application/json\n';
-    
+
     print(string.format('Getting data from %s', url));
     http.get(url, nil, getResponseHandler(expectedStatus, finishedCallback));
 end
 
 function post(baseUrl, urlPath, body, expectedStatus, finishedCallback)
     local url = string.format('%s/%s', baseUrl, urlPath);
-    -- local headers = 'Content-Type: application/json\nHost: orange-sensors-web.us-east-1.elasticbeanstalk.com\n';
     local headers = 'Content-Type: application/json\n';
     
     print(string.format('Sending data to %s', url));
     local body = sjson.encode(body);
     print('Body', body);
 
---    if (false) then
     http.post(url, headers, body, getResponseHandler(expectedStatus, finishedCallback));
---    end
 end
 
 return {
