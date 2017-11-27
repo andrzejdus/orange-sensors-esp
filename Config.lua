ca0a5bc926c26889058a6e73224055eeabf3ea3e
Andrzej Duś <andrzej.dus@gmail.com>
2017-11-27 23:07:21 +0100

    Update light pins

--- Config.lua
+++ Config.lua
@@ -2,8 +2,8 @@ return {
     BASE_URL = 'http://orange-sensors-web.ds1.pl:8000',
     OTA_URL = 'http://raw.githubusercontent.com/andrzejdus/orange-sensors-esp/master',
     STATUS_DIODE_PIN = 1, -- GPIO5
-    GREEN_LIGHT_PIN = 4,
-    RED_LIGHT_PIN = 8,
+    GREEN_LIGHT_PIN = 0, -- GPI16
+    RED_LIGHT_PIN = 5, -- GPIO14
     TRIGGER_PIN = 6, -- GPIO12
     ECHO_PIN = 7 -- GPIO13 
 };
