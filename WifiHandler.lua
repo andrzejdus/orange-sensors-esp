f8084829b215f3e985892dee682bdb044c86cff8
Andrzej Duś <andrzej.dus@gmail.com>
2017-12-05 22:06:39 +0100

    Fix broken logic in WifiHandler

--- WifiHandler.lua
+++ WifiHandler.lua
@@ -9,14 +9,14 @@ function WifiHandler.init(wifiReadyCallback, wifiInteruppedCallback)
     function wifiConnected(ssid)
         print(string.format('Wi-Fi "%s" is connected', ssid));
     end
-    
+
     function gotIp(ip, netmask, gateway)
         print(string.format('IP is: %s, netmask: %s, gateway: %s', ip, netmask, gateway));
 
         statusDiode.turnOn();
-        wifiReadyCallback();        
+        wifiReadyCallback();
     end
-    
+
     function wifiDisconnect(disconnectReason)
         print(string.format('Wi-Fi disconnected, reason %s', disconnectReason));
 
@@ -24,26 +24,27 @@ function WifiHandler.init(wifiReadyCallback, wifiInteruppedCallback)
         wifiInteruppedCallback();
     end
 
+    wifi.eventmon.register(wifi.eventmon.STA_CONNECTED, function(T)
+        wifiConnected(T.SSID);
+    end);
+
+    wifi.eventmon.register(wifi.eventmon.STA_GOT_IP, function(T)
+        gotIp(wifi.sta.getip(), T.netmask, T.gateway);
+    end);
+
+    wifi.eventmon.register(wifi.eventmon.STA_DISCONNECTED, function(T)
+        wifiDisconnect(T.reason);
+    end);
+
     if (wifi.sta.status() == 5) then
         print('WiFi already connected');
         wifiConnected(wifi.sta.getconfig(true).ssid);
         local ip, netmask, gateway = wifi.sta.getip();
         gotIp(ip, netmask, gateway)
+        wifiReadyCallback();
     else
         print('Waiting for WiFi...');
         statusDiode.blink();
-
-        wifi.eventmon.register(wifi.eventmon.STA_CONNECTED, function(T)
-            wifiConnected(T.SSID);
-        end);
-
-        wifi.eventmon.register(wifi.eventmon.STA_GOT_IP, function(T)
-            gotIp(wifi.sta.getip(), T.netmask, T.gateway);
-        end);
-        
-        wifi.eventmon.register(wifi.eventmon.STA_DISCONNECTED, function(T)
-            wifiDisconnect(T.reason);
-        end);
     end
 
     local noWifiCount = 0;
@@ -57,12 +58,12 @@ function WifiHandler.init(wifiReadyCallback, wifiInteruppedCallback)
             local noWifiSeconds = noWifiCount * wifiCheckInterval / 1000;
 
             print(string.format('Wifi not connected (status %s) for %d seconds', wifi.sta.status(), noWifiSeconds));
-    
+
             if (noWifiSeconds >= 10) then
                 print('Reconnecting wifi');
                 wifi.sta.disconnect();
                 wifi.sta.connect();
-    
+
                 noWifiCount = 0;
             end
         else
