4891539ba4433ee923bf834b5e66819538baf5e7
Andrzej Duś <andrzej.dus@gmail.com>
2017-12-05 22:44:07 +0100

    Get rid of global functions in WifiHandler

--- WifiHandler.lua
+++ WifiHandler.lua
@@ -3,45 +3,46 @@ local Config = require 'Config';
 
 local WifiHandler = {};
 
+local function wifiConnected(ssid)
+    print(string.format('Wi-Fi "%s" is connected', ssid));
+end
+
+local function gotIp(ip, netmask, gateway, statusDiode, wifiReadyCallback)
+    print(string.format('IP is: %s, netmask: %s, gateway: %s', ip, netmask, gateway));
+
+    statusDiode.turnOn();
+    wifiReadyCallback();
+end
+
+local function wifiDisconnect(disconnectReason, statusDiode, wifiInteruppedCallback)
+    print(string.format('Wi-Fi disconnected, reason %s', disconnectReason));
+
+    statusDiode.blink();
+    wifiInteruppedCallback();
+end
+
 function WifiHandler.init(wifiReadyCallback, wifiInteruppedCallback)
     local statusDiode = Light.create(Config.STATUS_DIODE_PIN);
-
-    function wifiConnected(ssid)
-        print(string.format('Wi-Fi "%s" is connected', ssid));
-    end
-
-    function gotIp(ip, netmask, gateway)
-        print(string.format('IP is: %s, netmask: %s, gateway: %s', ip, netmask, gateway));
-
-        statusDiode.turnOn();
-        wifiReadyCallback();
-    end
-
-    function wifiDisconnect(disconnectReason)
-        print(string.format('Wi-Fi disconnected, reason %s', disconnectReason));
-
-        statusDiode.blink();
-        wifiInteruppedCallback();
-    end
 
     wifi.eventmon.register(wifi.eventmon.STA_CONNECTED, function(T)
         wifiConnected(T.SSID);
     end);
 
     wifi.eventmon.register(wifi.eventmon.STA_GOT_IP, function(T)
-        gotIp(wifi.sta.getip(), T.netmask, T.gateway);
+        gotIp(wifi.sta.getip(), T.netmask, T.gateway, statusDiode, wifiReadyCallback);
     end);
 
     wifi.eventmon.register(wifi.eventmon.STA_DISCONNECTED, function(T)
-        wifiDisconnect(T.reason);
+        wifiDisconnect(T.reason, statusDiode, wifiInteruppedCallback);
     end);
 
     if (wifi.sta.status() == 5) then
         print('WiFi already connected');
+
         wifiConnected(wifi.sta.getconfig(true).ssid);
+
         local ip, netmask, gateway = wifi.sta.getip();
-        gotIp(ip, netmask, gateway)
-        wifiReadyCallback();
+        gotIp(ip, netmask, gateway, statusDiode, wifiReadyCallback)
     else
         print('Waiting for WiFi...');
         statusDiode.blink();
